29b312d9ef8a27b5031e7fde5c8d5ef0fd269382
kaushal18 <kaushal.y1801@gmail.com>
2022-07-16 14:02:55 +0530

    path-sum problem

---
+++ Trees/path-sum.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/path-sum/
+
+class Solution {
+  public boolean hasPathSum(TreeNode root, int targetSum) {
+    return helper(root, targetSum, 0);
+  }
+    
+  public boolean helper(TreeNode root, int targetSum, int currentSum) {
+		if(root == null) return false;
+		
+		currentSum += root.val;
+		
+		if(root.left == null && root.right == null && currentSum == targetSum) {
+				return true;
+		}
+		
+		boolean isLeftTreeHasSum = helper(root.left, targetSum, currentSum);
+		boolean isRightTreeHasSum = helper(root.right, targetSum, currentSum);
+		
+		return isLeftTreeHasSum || isRightTreeHasSum;
+	}
+}
+
+class Solution {
+	public boolean hasPathSum(TreeNode root, int targetSum) {
+		if(root == null) return false;
+		
+		if(root.left == null && root.right == null) {
+				if(targetSum == root.val) return true;
+				else return false;
+		}
+		
+		boolean hasLeftPathSum = hasPathSum(root.left, targetSum - root.val);
+		boolean hasRightPathSum = hasPathSum(root.right, targetSum - root.val);
+		
+		return hasLeftPathSum || hasRightPathSum;
+	}
+}
