5f6b77a4ff6f9a61d7411c33e331c7f8032db351
kaushal18 <kaushal.y1801@gmail.com>
2022-08-28 20:13:39 -0700

    find-all-duplicates-in-an-array (can modify input)

---
+++ Leetcode/Array/medium/find-all-duplicates-in-an-array.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/find-all-duplicates-in-an-array/
+
+// negative marking solution
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        
+        for(int i=0; i<nums.length; i++) {
+            int idx = Math.abs(nums[i])-1;
+            if(nums[idx] < 0) {
+                ans.add(Math.abs(nums[i]));
+            }
+            else {
+                nums[idx] = -nums[idx];
+            }
+        }
+        
+        return ans;
+    }
+}
