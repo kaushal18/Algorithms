d611dff140c9fab8f2122bce28eaf77cfed877f7
kaushal18 <kaushal.y1801@gmail.com>
2023-05-10 01:15:16 -0700

    sort colors

---
+++ Blind75/Arrays and Hashing/sort-colors.py
@@ -0,0 +1,26 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # 3 pointers technique
+        # if mid = 0, swap mid with left and increment both (as both the values will be at correct position)
+        # if mid = 1, just increment the mid
+        # if mid = 2, swap mid with right, only decrement right (as the new mid value might be out of place)
+
+        left = 0
+        right = len(nums)-1
+        mid = 0
+
+        while mid <= right:
+            if nums[mid] == 0:
+                nums[left], nums[mid] = nums[mid], nums[left]
+                left += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[right] = nums[right], nums[mid]
+                right -= 1
+
+        
