17ef2e8a597b519fbe88b513cd27bb8064ae6e6c
kaushal18 <kaushal.y1801@gmail.com>
2023-05-15 17:07:32 -0700

    smallest-number-in-infinite-set design python

---
+++ Leetcode/Design/smallest-number-in-infinite-set.py
@@ -0,0 +1,66 @@
+
+# With SortedSet
+from sortedcontainers import SortedSet
+
+class SmallestInfiniteSet:
+    minHeap = None
+    minCounter = None
+
+    def __init__(self):
+        self.sortedSet = SortedSet()
+        self.minCounter = 1
+
+    # O(log N)
+    def popSmallest(self) -> int:
+        if len(self.sortedSet) > 0 and self.sortedSet[0] <= self.minCounter:
+            if self.sortedSet[0] == self.minCounter:
+                self.minCounter += 1
+            
+            _min = self.sortedSet[0]
+            self.sortedSet.discard(_min)
+            
+        else:
+            _min = self.minCounter
+            self.minCounter += 1
+        
+        return _min
+        
+    # O(log N)
+    def addBack(self, num: int) -> None:
+        if num < self.minCounter:
+            self.sortedSet.add(num)
+
+
+# With Min Heap + Set
+class SmallestInfiniteSet:
+    minHeap = None
+    minCounter = None
+    seenSet = None
+
+    def __init__(self):
+        self.minHeap = []
+        heapify(self.minHeap)
+        self.seenSet = set()
+        self.minCounter = 1
+
+    # O(1)
+    def popSmallest(self) -> int:
+        if len(self.minHeap) > 0 and self.minHeap[0] <= self.minCounter:
+            if self.minHeap[0] == self.minCounter:
+                self.minCounter += 1
+            _min = heappop(self.minHeap)
+            self.seenSet.remove(_min)          
+            
+        else:
+            _min = self.minCounter
+            self.minCounter += 1
+        
+        return _min
+        
+    # O(log N)
+    def addBack(self, num: int) -> None:
+        if num < self.minCounter:
+            if num not in self.seenSet:
+                heappush(self.minHeap, num)
+            self.seenSet.add(num)
+
